470dbf363acdca33db83309bd2c2a828d4691101
Dima Tsoy <dimktsoy@yahoo.com>
2020-05-17 20:23:31 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      result: null,
+    };
+  }
+
+  render() {
+    const { result } = this.state;
+
+    if (!result) {
+      return null;
+    }
+
+    return (
+      <div className="app">
+        <header className="app__header">
+          <div className="app__container">
+            header
+          </div>
+        </header>
+        <div className="app__content">
+          <div className="app__container">
+            content
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
