e3957bf897af8ec2d1c736cd29dffc038490c39e
Dima Tsoy <dimktsoy@yahoo.com>
2020-06-01 21:08:33 +0600

    Add Input component

---
+++ src/components/Input/index.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+import './index.scss';
+
+const Input = ({
+  id, className, label, ...attrs
+}, ref) => {
+  const classes = classNames(
+    'input__control',
+    className,
+  );
+
+  return (
+    <div className="input">
+      {label && <label className="input__label" htmlFor={id}>{label}</label>}
+      <input
+        ref={ref}
+        id={id}
+        className={classes}
+        // eslint-disable-next-line react/jsx-props-no-spreading
+        {...attrs}
+      />
+    </div>
+  );
+};
+
+Input.propTypes = {
+  id: PropTypes.string.isRequired,
+  className: PropTypes.string,
+  label: PropTypes.string,
+};
+
+Input.defaultProps = {
+  className: '',
+  label: '',
+};
+
+const forwaredInput = React.forwardRef(Input);
+
+export default forwaredInput;
