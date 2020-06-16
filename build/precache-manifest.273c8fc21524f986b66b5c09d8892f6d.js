0293f5a0b6b351a4988eeb3fca8ffbe5f95e70e9
Dima Tsoy <dimktsoy@yahoo.com>
2020-06-16 10:42:49 +0600

    Add build

---
+++ build/precache-manifest.273c8fc21524f986b66b5c09d8892f6d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70edc42197a787076b1c0d4037d849b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e61583dbd7004fae056",
+    "url": "/static/css/main.6df8af04.chunk.css"
+  },
+  {
+    "revision": "abb5b2da2b3a4afecf20",
+    "url": "/static/js/2.e1c19e4d.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/static/js/2.e1c19e4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e61583dbd7004fae056",
+    "url": "/static/js/main.3c82baba.chunk.js"
+  },
+  {
+    "revision": "ff04d0546b174db808d6",
+    "url": "/static/js/runtime-main.0eb35775.js"
+  },
+  {
+    "revision": "2844ec013ad503af74b07f586d5020dc",
+    "url": "/static/media/PTSans-Bold.2844ec01.woff"
+  },
+  {
+    "revision": "5fb7ebc602fa8ffe3bd0584a5b8ffec5",
+    "url": "/static/media/PTSans-Bold.5fb7ebc6.woff2"
+  },
+  {
+    "revision": "21c776365beba0f1ff78315711b43aea",
+    "url": "/static/media/PTSans-Regular.21c77636.woff2"
+  },
+  {
+    "revision": "8f69eec893df9a78a9695b3ce4d349f9",
+    "url": "/static/media/PTSans-Regular.8f69eec8.woff"
+  }
+]);
